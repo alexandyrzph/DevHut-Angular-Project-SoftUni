cc1b3ddbd9600a45a57594972062127b9cbb9969
alexandyrzph <alexandyrzph@gmail.com>
2022-04-05 16:40:44 +0300

    Refactoring firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -23,8 +20,6 @@ import { ArticleModule } from './features/articles/article.module';
         AuthModule,
         CoreModule,
         SharedModule,
-        AngularFireModule.initializeApp(environment.firebaseConfig),
-        AngularFirestoreModule,
         ArticleModule
     ],
     providers: [],
--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -7,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    imgPath: string = 'https://th.bing.com/th/id/R.124df381aa1358a940624eeb9693bff6?rik=WwSQ%2fNthGGEZIA&riu=http%3a%2f%2fwww.wallpapersin4k.org%2fwp-content%2fuploads%2f2016%2f12%2fHacking-Wallpapers-7.jpg&ehk=uyMLWZRbLxaMcW2Rft204D6dBEcfkrrOoUch00ZZSq0%3d&risl=&pid=ImgRaw&r=0';
-
     constructor() { }
 
     ngOnInit(): void {
