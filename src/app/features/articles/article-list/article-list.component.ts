e43ab451fe0edac87cc147744d76eda91556f724
alexandyrzph <alexandyrzph@gmail.com>
2022-04-30 10:30:11 +0300

    Cleanup

--- src/app/features/articles/article-list/article-list.component.ts
+++ src/app/features/articles/article-list/article-list.component.ts
@@ -1,25 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../../../core/services/crud.service';
 import { Article } from '../../../shared/models/article.model';
-import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-articles-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css'],
-  animations: [
-    trigger('enterAnimation', [
-      transition(':enter', [
-        query('.fadeinAnim', [
-          style({ opacity: 0, transform: 'translateX(-20px)' }),
-          stagger('400ms', [
-            animate('500ms 0.5s ease-in-out',
-              style({ opacity: 1, transform: 'translateY(0)' }))
-          ])
-        ], { optional: true }),
-      ])
-    ])
-  ]
 })
 export class ArticleListComponent implements OnInit {
   articles: Article[] | any;
